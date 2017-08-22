6c674687b49e115a58ffc4b6c89698abeb922072
Mark Bridges <mark@bridgetech.io>
2017-08-22 09:25:17 +0100

    Initial Commit

---
+++ MacPrompter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacPrompter
+//
+//  Created by Mark Bridges on 22/08/2017.
+//  Copyright © 2017 Mark Bridges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
