d39d774f93b246d709aaab7858bebb1e708c9575
Mark Bridges <mark@bridgetech.io>
2018-09-15 20:09:55 +0100

    Swift 4.2

--- MacPrompter.podspec
+++ MacPrompter.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
 s.name              = 'MacPrompter'
-s.version           = '0.0.9'
+s.version           = '0.0.10'
 s.summary           = 'Prompts the user to rate the app or view your other apps.'
 s.homepage          = 'https://github.com/Megatron1000/MacPrompter'
 s.license           = {
@@ -13,7 +13,7 @@ s.author            = {
 }
 s.source            = {
 :git => 'https://github.com/Megatron1000/MacPrompter.git',
-:tag => "0.0.9"
+:tag => "0.0.10"
 }
 s.platform     = :osx, '10.10'
 s.source_files = 'MacPrompter/Classes/**/*.{swift, strings}'
--- MacPrompter/Classes/MutableCollection+Shuffling.swift
+++ MacPrompter/Classes/MutableCollection+Shuffling.swift
@@ -14,18 +14,18 @@ extension MutableCollection {
         let c = count
         guard c > 1 else { return }
         
-        for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
-            guard d != 0 else { continue }
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
             let i = index(firstUnshuffled, offsetBy: d)
-            self.swapAt(firstUnshuffled, i)
+            swapAt(firstUnshuffled, i)
         }
     }
 }
 
 extension Sequence {
     /// Returns an array with the contents of this sequence, shuffled.
-    func shuffled() -> [Iterator.Element] {
+    func shuffled() -> [Element] {
         var result = Array(self)
         result.shuffle()
         return result
